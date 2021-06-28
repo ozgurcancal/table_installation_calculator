1d520c2720941ba5f66007f36151d2165d9f8fa7
ozgurcancal <ozgryalcinee@hotmail.com>
2021-06-28 16:49:57 +0300

    redundant comments are deleted

--- main.cpp
+++ main.cpp
@@ -36,12 +36,5 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-    //for (auto it = begin(ivec); it != end(ivec); ++it) {
-    //    
-    //}
 
 
-/*
-    opengl ile canlandurma yap : bir düzlem oluştur altta kalanlar kırmızı olsun
-
-*/
